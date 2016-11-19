f4aa2418b0988482c00f81ebdbaeb88398ba46c0
vikashupadhyay <vikasku@thoughtworks.com>
2016-11-19 12:34:52 +0530

    tree parser tested

--- js/treeParser.js
+++ js/treeParser.js
@@ -1,13 +1,33 @@
-class TreeParser{
-	constructor(parantNode,leftChild,rightChild){
-		this.parantNode = parantNode;
-		this.leftChild = leftChild;
-		this.rightChild = rightChild;
-	}
+var numberToWords = require("number-to-words");
+class TreeParser {
+    constructor(parantNode, leftChild, rightChild) {
+        this.parantNode = parantNode;
+        this.leftChild = leftChild;
+        this.rightChild = rightChild;
+    }
 
-	toString(){
-		return '(' + this.leftChild + this.parantNode + this.rightChild + ')';
-	}
+    toString() {
+
+        if (this.leftChild instanceof TreeParser && this.rightChild instanceof TreeParser) {
+            return '(' + this.leftChild.toString() + ' ' + TreeParser.getOperator(this.parantNode) + ' ' + this.rightChild.toString() + ')';
+        }
+
+        if (this.rightChild instanceof TreeParser && typeof this.leftChild == 'number') {
+            return '(' + numberToWords.toWords(this.leftChild) + ' ' + TreeParser.getOperator(this.parantNode) + ' ' + this.rightChild.toString() + ')';
+        }
+
+        if (this.leftChild instanceof TreeParser && typeof this.rightChild == 'number') {
+            return '(' + this.leftChild.toString() + ' ' + TreeParser.getOperator(this.parantNode) + ' ' + numberToWords.toWords(this.rightChild) + ')';
+        }
+
+
+        return '(' + numberToWords.toWords(this.leftChild) + " " + TreeParser.getOperator(this.parantNode) + " " + numberToWords.toWords(this.rightChild) + ')';
+    }
+
+    static getOperator(operator) {
+        var operators = {"+": "plus", "*": "minus"};
+        return operators[operator];
+    }
 }
 
 module.exports = TreeParser;
--- test/treeParserTest.js
+++ test/treeParserTest.js
@@ -1,10 +1,37 @@
 var assert = require("assert");
-var chai  = require("chai");
+var chai = require("chai");
 var TreeParser = require("../js/treeParser.js");
 
-describe("tree parser",function() {
-	it("should return string form object",function(){
-        var treeParser = new TreeParser("+","2","3");
-        chai.expect(treeParser.toString()).to.equal("(2+3)");
-	})
+describe("tree parser", function () {
+    it("should return string representation of 2+3", function () {
+        var treeParser = new TreeParser("+", 2, 3);
+        chai.expect(treeParser.toString()).to.equal("(two plus three)");
+    });
+
+    it("should return string representation of 3+(2+3)", function () {
+        var childTree = new TreeParser("+", 2, 3);
+        var parantTree = new TreeParser("+", 3, childTree);
+        chai.expect(parantTree.toString()).to.equal("(three plus (two plus three))");
+    });
+
+    it("should return string representation of ((3+2)+3)", function () {
+        var childTree = new TreeParser("+", 3, 2);
+        var parantTree = new TreeParser("+", childTree, 3);
+        chai.expect(parantTree.toString()).to.equal("((three plus two) plus three)");
+    });
+
+    it("should return string representation of ((3+2)+(3+4))", function () {
+        var childTree1 = new TreeParser("+", 3, 2);
+        var childTree2 = new TreeParser("+", 3, 4);
+        var parantTree = new TreeParser("+", childTree1, childTree2);
+        chai.expect(parantTree.toString()).to.equal("((three plus two) plus (three plus four))");
+    });
+
+    it("should return string representation of ((3+2)+(3+(4+2)))", function () {
+        var leftChildForParantNode = new TreeParser("+", 3, 2);
+        var childTree = new TreeParser("+", 4, 2);
+        var rightChildForParantNode = new TreeParser("+", 3, childTree);
+        var parantTree = new TreeParser("+", leftChildForParantNode, rightChildForParantNode);
+        chai.expect(parantTree.toString()).to.equal("((three plus two) plus (three plus (four plus two)))");
+    })
 });
