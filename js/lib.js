b443ef107b2d58c131e31e5772973323a5b541a6
vikashupadhyay <vikasku@thoughtworks.com>
2016-11-27 14:18:59 +0530

    #0 - vikas - word representation and string representation of exp is implemented.

--- js/lib.js
+++ js/lib.js
@@ -1,11 +1,24 @@
-var Tree = require('../js/treeParser');
+var Tree = require('./tree');
+var Node = require('./node');
 class Lib {
 
-    static parenthesis(tree) {
-        var withParanthesis = [];
+    static parenthesis(trees) {
+        return this.parenthesisForExp(trees[0]);
+    }
+
+    static changeToWords(trees) {
+        return this.changeToWordsForExp(trees[0]);
+    }
+
+    static isEqualOperator(eachTree) {
+        return !(eachTree instanceof Node) && eachTree.parantNode.value == '=';
+    };
+
+    static parenthesisForExp(tree) {
+        var withParanthesis = '';
         for (var node in tree) {
             if (tree[node] instanceof Tree) {
-                withParanthesis += this.parenthesis(tree[node]);
+                withParanthesis += this.parenthesisForExp(tree[node]);
             } else {
                 withParanthesis += tree[node].value;
             }
@@ -13,17 +26,39 @@ class Lib {
         return `(${withParanthesis})`;
     }
 
-    static changeToWords(tree) {
+    static changeToWordsForExp(tree) {
         var inWords = [];
         for (var node in tree) {
             if (tree[node] instanceof Tree) {
-                inWords.push(this.changeToWords(tree[node]));
+                inWords.push(this.changeToWordsForExp(tree[node]));
             } else {
                 inWords.push(tree[node].toWords());
             }
         }
         return `(${inWords.join(" ")})`;
     }
+
+    static lookupTable(tree) {
+        var table = {};
+        let self = this;
+        tree.forEach(function (eachTree) {
+            if (self.isEqualOperator(eachTree))
+                table[eachTree.leftChild.value] = eachTree.rightChild.evaluate();
+        });
+        return table;
+    }
+
+    static  evaluate(tree) {
+
+        var lookupTable = this.lookupTable(tree);
+        // if (tree.leftChild instanceof Tree)
+        //     this.evaluate(tree.leftChild, lookupTable);
+        // if (tree.rightChild instanceof Tree)
+        //     this.evaluate(tree.rightChild, lookupTable);
+        // return tree.parantNode.evaluate(tree.leftChild, tree.rightChild, lookupTable);
+    }
+
 }
+
 
 module.exports = Lib;
--- test/treeParserTest.js
+++ test/treeParserTest.js
@@ -10,53 +10,76 @@ var lib = require('../js/lib');
 describe("tree parser", function () {
 
     it("should return string representation of 2+3", function () {
-        var tree = parser.parse("2+3");
+        var tree = parser.parse("2+3;");
         chai.expect(lib.parenthesis(tree)).to.equal("(2+3)");
     });
 
     it("should return string representation of 3+2+3", function () {
-        var tree = parser.parse("3+2+3");
+        var parser = new Parser(grammar);
+        var tree = parser.parse("3+2+3;");
         chai.expect(lib.parenthesis(tree)).to.equal("(3+(2+3))");
     });
 
     it("should return string representation of 3+2+3+4", function () {
-        var tree = parser.parse("3+2+3+4");
+        var tree = parser.parse("3+2+3+4;");
         chai.expect(lib.parenthesis(tree)).to.equal("(3+(2+(3+4)))");
     });
 
     it("should return word representation of 2+3", function () {
-        var tree = parser.parse("2+3");
+        var tree = parser.parse("2+3;");
         chai.expect(lib.changeToWords(tree)).to.equal("(two plus three)");
     });
 
-    it("should return string representation of 1000000000+2", function () {
-        var tree = parser.parse("1000000000+2");
+    it("should return word representation of 1000000000+2", function () {
+        var tree = parser.parse("1000000000+2;");
         chai.expect(lib.changeToWords(tree)).to.equal("(one billion plus two)");
     });
 
     it("should return word representation of 2*3", function () {
-        var tree = parser.parse("2*3");
+        var tree = parser.parse("2*3;");
         chai.expect(lib.changeToWords(tree)).to.equal("(two times three)");
     });
     it("should return string representation of 2*3", function () {
-        var tree = parser.parse("2*3");
+        var tree = parser.parse("2*3;");
         chai.expect(lib.parenthesis(tree)).to.equal("(2*3)");
     });
     it("should return string representation of 2+3*4", function () {
-        var tree = parser.parse("2+3*4");
+        var tree = parser.parse("2+3*4;");
         chai.expect(lib.parenthesis(tree)).to.equal("(2+(3*4))");
     });
     it("should return string representation of 3*4+4", function () {
-        var tree = parser.parse("3*4+4");
+        var tree = parser.parse("3*4+4;");
         chai.expect(lib.parenthesis(tree)).to.equal("((3*4)+4)");
     });
     it("should return word representation of 3*4+4", function () {
-        var tree = parser.parse("3*4+4");
+        var tree = parser.parse("3*4+4;");
         chai.expect(lib.changeToWords(tree)).to.equal("((three times four) plus four)");
+    });
+    it("should return word representation of 3*4+4*5", function () {
+        var tree = parser.parse("3*4+4*5;");
+        chai.expect(lib.changeToWords(tree)).to.equal("((three times four) plus (four times five))");
     });
 
     it("should return word representation of 2+3*4", function () {
-        var tree = parser.parse("2+3*4");
+        var tree = parser.parse("2+3*4;");
         chai.expect(lib.changeToWords(tree)).to.equal("(two plus (three times four))");
     });
+
+    it("should create lookupTable for : a=2;b=3;a+b;", function () {
+        var tree = parser.parse("a=2;b=3;a+b;");
+        var table = lib.lookupTable(tree);
+        chai.expect(table["a"]).to.equal(2);
+        chai.expect(table["b"]).to.equal(3);
+    });
+    it("should create lookupTable for : a;a=2;", function () {
+        var tree = parser.parse("a=2;a;");
+        var table = lib.lookupTable(tree);
+        chai.expect(table["a"]).to.equal(2);
+    });
+
+    it("should evaulate 2+3;", function () {
+        var tree = parser.parse("2+3;");
+        chai.expect(lib.evaluate(tree)).to.equal(2);
+    });
+
 });
